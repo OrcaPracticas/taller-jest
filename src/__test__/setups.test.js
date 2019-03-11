3938473032d6da9e00e8c8474d805ab28862b28b
Jmendez <jorge.mendez.ortega@gmail.com>
2019-03-10 23:11:39 -0600

    ADD: Jest nos proporciona funciones auxiliares para mejorar el manejo de nuestras pruebas, esto significa. Que podremos correr funciones antes, durante o después de nuestras pruebas.
    Esto nos permite setear ciertas configuraciones muy particulares que 
    necesitemos antes que corra la prueba o después de esta.

---
+++ src/__test__/setups.test.js
@@ -0,0 +1,21 @@
+afterEach(() => {
+    console.log("Despues de cada prueba");
+});
+
+afterAll(() => {
+    console.log("Despues de todas las pruebas");
+});
+
+beforeEach(() => {
+    console.log("Antes de cada prueba");
+});
+
+beforeAll(() => {
+    console.log("Antes de todas las pruebas");
+});
+
+describe("preparar antes de ejecutar", () => {
+    test("Es verdadero", () => {
+        expect(true).toBeTruthy();
+    });
+});
